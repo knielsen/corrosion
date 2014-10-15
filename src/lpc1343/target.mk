add372be40f5fcdddf756381d2047e81ea494d9f
Emil Renner Berthing <esmil@mailme.dk>
2014-10-16 01:06:07 +0200

    fix lpc1343

--- src/lpc1343/target.mk
+++ src/lpc1343/target.mk
@@ -3,5 +3,9 @@ CPU = cortex-m3
 MCOPY = mcopy
 DEST = a:
 
-upload: $(NAME).bin
+fix-checksum: src/fix-checksum.c
+	$(HOSTCC) -O2 -Wall -Wextra -pipe $< -o $@
+
+upload: $(NAME).bin fix-checksum
+	./fix-checksum $<
 	$(MCOPY) $< $(DEST)firmware.bin
