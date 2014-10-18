2dc7222ef07bf011d931a42fa6c4222c98db924a
Emil Renner Berthing <esmil@mailme.dk>
2014-10-18 18:42:46 +0200

    tm4c123x/target.mk: add stty and cat targets

--- src/tm4c123x/target.mk
+++ src/tm4c123x/target.mk
@@ -1,6 +1,16 @@
 CPU = cortex-m4
 
+SPORT = /dev/ttyACM0
+
+STTY = stty
+CAT = cat
 LM4FLASH = lm4flash
+
+stty: $(SPORT)
+	$(STTY) -F $(SPORT) raw cs8 -parenb -cstopb 115200
+
+cat: $(SPORT)
+	$(CAT) $(SPORT)
 
 upload: $(NAME).bin
 	$(LM4FLASH) $<
